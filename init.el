df173492abfa755cf0c4fd93e30961c6cb7b14d6
qinains <qinains@qq.com>
2017-05-05 23:16:49 +0800

    重构，使之启动更快

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; init.el --- Clojure 开发常用配置
+;;; init.el --- 开发常用配置
 
 ;;; Commentary:
 ;; 请查看 README.md 文件
@@ -136,136 +136,6 @@
   (untabify (point-min) (point-max))
   (indent-region (point-min) (point-max) nil))
 
-(add-hook 'after-init-hook
-          (lambda ()
-            (when (not (version< emacs-version "24.1"))
-
-              ;; 设置字体
-              (set-frame-font "-outline-WenQuanYi Micro Hei Mono-normal-normal-normal-sans-13-*-*-*-p-*-iso8859-1")
-              (tool-bar-mode -1)
-              (scroll-bar-mode -1)
-              (electric-indent-mode)
-              (global-linum-mode))
-
-            ;; 如果是windows系统
-            (if (memq window-system '(w32))
-                (progn
-                  ;; 修复https的git push不了的问题
-                  (setenv "GIT_ASKPASS" "git-gui--askpass")))
-
-            ;; 光标显示为一竖线
-            (setq-default cursor-type 'bar)
-
-            ;; 设置标题栏显示文件的完整路径名
-            (setq frame-title-format
-                  '("%S" (buffer-file-name "%f" (dired-directory dired-directory "%b"))))
-
-            ;; C-x C-f后默认打开的文件夹
-            (setq default-directory "~/")
-
-            ;; 当要回答yes或no时，直接输入y或n
-            (fset 'yes-or-no-p 'y-or-n-p)
-
-            ;; 编码
-            ;; 显示当前文件编码，C-h C (或者M-x describe-current-coding-system)
-            ;; 如果打开的文件有乱码，还原成文件默认编码：C-x <RET> r <RET> (或者 M-x revert-buffer-with-coding-system)
-            ;; 如果想转码，改变当前buffer的编码为UTF-8：C-x <RET> f utf-8 （或者 M-x set-buffer-file-coding-system）
-            (prefer-coding-system 'utf-8)
-            ;; 新建的文件都保存成UTF-8编码
-            (setq buffer-file-coding-system 'utf-8)
-            ;; tab键和新行自动缩进
-            (setq-default indent-tabs-mode nil)
-            (setq-default tab-width 4)
-            (setq tab-width 4)
-            (setq tab-stop-list ())
-            (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
-
-            (setq column-number-mode t)
-            (setq size-indication-mode t)
-            (setq visible-bell t)
-
-            ;; C-c,C-v,C-x,C-z复制、粘贴、剪切、撤销
-            (transient-mark-mode)
-            (cua-mode)
-            (setq cua-auto-tabify-rectangles nil)
-            (setq cua-keep-region-after-copy t)
-            (setq x-select-enable-clipboard t)
-            (setq mouse-yank-at-point t)
-
-            ;; Fix problem with cua-delete-region in Emacs 24.4
-            ;; 参考http://pastebin.com/sDNqakF3
-            (unless (fboundp 'cua-replace-region)
-              (defun cua-replace-region ()
-                "Replace the active region with the character you type."
-                (interactive)
-                (let ((not-empty (and cua-delete-selection (cua-delete-region))))
-                  (unless (eq this-original-command this-command)
-                    (let ((overwrite-mode
-                           (and overwrite-mode
-                                not-empty
-                                (not (eq this-original-command 'self-insert-command)))))
-                      (cua--fallback))))))
-
-            ;; 选择文字后输入文字，不再追加，而是直接替换
-            (delete-selection-mode)
-
-            (show-paren-mode)
-
-            ;; 文件有更改时自动更新
-            (global-auto-revert-mode)
-
-            ;; 按键绑定
-            (global-set-key (kbd "RET") 'newline-and-indent)
-            (global-set-key (kbd "C-<return>") 'newline)
-
-            ;; F4键弹出eshell
-            (global-set-key [f4] 'eshell)
-
-            ;; 按Shift+方向键即可切换窗口
-            (when (fboundp 'windmove-default-keybindings)
-              (windmove-default-keybindings))
-
-            ;;按C-M-\键格式化
-            (global-set-key (kbd "C-M-\\") 'indent-whole)))
-
-(add-hook 'go-mode-hook
-          (lambda ()
-            (setq gofmt-command "goimports")
-            (add-hook 'before-save-hook 'gofmt-before-save)
-
-            (setq company-tooltip-limit 20)
-            (setq company-idle-delay .25)
-            (setq company-echo-delay 0)
-            (setq company-begin-commands '(self-insert-command))
-            (set (make-local-variable 'company-backends) '(company-go))
-            (company-mode)
-
-            (go-eldoc-setup)
-
-            (go-guru-hl-identifier-mode)
-
-            (require 'flycheck-gometalinter)
-            (setq flycheck-disabled-checkers '(go-gofmt
-                                               go-golint
-                                               go-vet
-                                               go-build
-                                               go-test
-                                               go-errcheck))
-            (eval-after-load 'flycheck
-              '(add-hook 'flycheck-mode-hook #'flycheck-gometalinter-setup))))
-
-(add-hook
- 'emacs-startup-hook
- (lambda()
-   (loaded-time "all-packages" emacs-start-time)))
-
-;; 记住关闭前光标的位置
-(require 'saveplace)
-(setq-default save-place t)
-
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-
 (defmacro after-load (name &rest body)
   "`eval-after-load' NAME evaluate BODY."
   (declare (indent defun))
@@ -275,107 +145,163 @@
                         ,@body
                         (loaded-time ,name ts))))))
 
-(after-load "adoc-mode-autoloads"
-  (add-to-list 'auto-mode-alist '("\\.adoc\\'" . adoc-mode)))
+(add-hook
+ 'emacs-startup-hook
+ (lambda()
+   (loaded-time "all-packages" emacs-start-time)))
 
-;; 开启自动补齐模式
-(after-load "company-autoloads"
-  (add-hook 'after-init-hook #'global-company-mode)
-  (setq company-dabbrev-downcase nil)
-  (setq company-dabbrev-ignore-case nil)
-  (setq company-minimum-prefix-length 1)
-  (setq company-show-numbers t)
-  (company-flx-mode)
-  (global-set-key "\t" 'company-complete-common))
+;; 全局性设置 start
 
-(after-load "cider-autoloads"
-  (setq cider-repl-wrap-history t)
-  (setq nrepl-log-messages t)
-  (setq cider-repl-history-size 3000)
-  (setq cider-repl-history-file "~/.emacs.d/cider-history")
-  (setq cider-refresh-show-log-buffer t)
-  (add-hook 'cider-mode-hook #'eldoc-mode)
-  (add-hook 'cider-repl-mode-hook #'subword-mode)
-  (global-set-key (kbd "C-c C-z") 'cider-switch-to-repl-buffer))
+;; 记住关闭前光标的位置
+(require 'saveplace)
+(setq-default save-place t)
 
-(after-load "clj-refactor"
-  (add-hook 'clojure-mode-hook
-            (lambda ()
-              (clj-refactor-mode)
-              (cljr-add-keybindings-with-prefix "C-c RET"))))
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
 
-;; Clojure
-(after-load "clojure-mode-autoloads"
-  (add-hook 'clojure-mode-hook 'subword-mode)
-  (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode))
+(add-hook
+ 'after-init-hook
+ (lambda ()
+   (message "after-init-hook")
+   (when (not (version< emacs-version "24.1"))
 
-(after-load "clojure-mode-extra-font-locking-autoloads"
-  (setq cider-repl-use-clojure-font-lock t))
+     ;; 设置字体
+     (set-frame-font "-outline-WenQuanYi Micro Hei Mono-normal-normal-normal-sans-13-*-*-*-p-*-iso8859-1")
 
-(after-load "flycheck-autoloads"
-  (add-hook 'after-init-hook (lambda ()
-                               (global-flycheck-mode)
-                               (flycheck-pos-tip-mode)
-                               (add-hook 'clojure-mode-hook
-                                         (lambda ()
-                                           (flycheck-clojure-setup))))))
+     (tool-bar-mode -1)
+     (scroll-bar-mode -1)
+     (electric-indent-mode)
+     (global-linum-mode))
 
-(after-load "flx-ido-autoloads"
-  (ido-mode)
-  (ido-everywhere)
-  (flx-ido-mode)
-  (setq ido-enable-flex-matching t)
-  (setq ido-use-faces nil))
+   ;; 如果是windows系统
+   (if (memq window-system '(w32))
+       (progn
+         ;; 修复https的git push不了的问题(私钥不能有密码)
+         (setenv "GIT_ASKPASS" "git-gui--askpass")))
 
-;; 按F12键启动magit-status
-(after-load "magit-autoloads"
-  (add-hook 'magit-mode-hook
-            ;;添加gitflow插件
-            (lambda ()
-              (require 'magit-gitflow)
-              (turn-on-magit-gitflow)))
-  (global-set-key [f12] 'magit-status))
+   ;; 光标显示为一竖线
+   (setq-default cursor-type 'bar)
 
-;;配置markdown插件
-(after-load "markdown-mode-autoloads"
-  (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-  (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-  (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-  (defun markdown-custom ()
-    "markdown-mode-hook"
-    (setq markdown-command "markdown"))
-  (add-hook 'markdown-mode-hook '(lambda() (markdown-custom))))
+   ;; 设置标题栏显示文件的完整路径名
+   (setq frame-title-format
+         '("%S" (buffer-file-name "%f" (dired-directory dired-directory "%b"))))
 
-(after-load "projectile-autoloads"
-  (projectile-global-mode))
+   ;; C-x C-f后默认打开的文件夹
+   (setq default-directory "~/")
 
-;; 括号高亮
-(after-load "rainbow-delimiters-autoloads"
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode-enable))
+   ;; 当要回答yes或no时，直接输入y或n
+   (fset 'yes-or-no-p 'y-or-n-p)
 
-(after-load "restclient-autoloads"
-  (add-to-list 'auto-mode-alist '("\\.rc\\'" . restclient-mode))
-  (add-to-list 'auto-mode-alist '("\\.restclient\\'" . restclient-mode)))
+   ;; 编码
+   ;; 显示当前文件编码，C-h C (或者M-x describe-current-coding-system)
+   ;; 如果打开的文件有乱码，还原成文件默认编码：C-x <RET> r <RET> (或者 M-x revert-buffer-with-coding-system)
+   ;; 如果想转码，改变当前buffer的编码为UTF-8：C-x <RET> f utf-8 （或者 M-x set-buffer-file-coding-system）
+   (prefer-coding-system 'utf-8)
+   ;; 新建的文件都保存成UTF-8编码
+   (setq buffer-file-coding-system 'utf-8)
+   ;; tab键和新行自动缩进
+   (setq-default indent-tabs-mode nil)
+   (setq-default tab-width 4)
+   (setq tab-width 4)
+   (setq tab-stop-list ())
+   (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
 
-(after-load "smartparens-autoloads"
-  (add-hook 'lisp-mode-hook #'smartparens-strict-mode)
-  (add-hook 'lisp-interaction-mode-hook #'smartparens-strict-mode)
-  (add-hook 'emacs-lisp-mode-hook #'smartparens-strict-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook #'smartparens-strict-mode)
-  (add-hook 'ielm-mode-hook #'smartparens-strict-mode)
-  (add-hook 'scheme-mode-hook #'smartparens-strict-mode)
-  (add-hook 'clojure-mode-hook #'smartparens-strict-mode)
-  (add-hook 'cider-repl-mode-hook #'smartparens-strict-mode))
+   (setq column-number-mode t)
+   (setq size-indication-mode t)
+   (setq visible-bell t)
 
-;; 扩展M-x功能
-(after-load "smex-autoloads"
-  (add-hook 'after-init-hook #'smex-initialize)
-  (global-set-key (kbd "M-x") 'smex)
-  (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-  ;; 原配的M-x
-  (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
+   ;; C-c,C-v,C-x,C-z复制、粘贴、剪切、撤销
+   (transient-mark-mode)
+   (cua-mode)
+   (setq cua-auto-tabify-rectangles nil)
+   (setq cua-keep-region-after-copy t)
+   (setq x-select-enable-clipboard t)
+   (setq mouse-yank-at-point t)
 
-;; 添加主题：白天黑夜自动切换主题，19:00自动切换
+   ;; Fix problem with cua-delete-region in Emacs 24.4
+   ;; 参考http://pastebin.com/sDNqakF3
+   (unless (fboundp 'cua-replace-region)
+     (defun cua-replace-region ()
+       "Replace the active region with the character you type."
+       (interactive)
+       (let ((not-empty (and cua-delete-selection (cua-delete-region))))
+         (unless (eq this-original-command this-command)
+           (let ((overwrite-mode
+                  (and overwrite-mode
+                       not-empty
+                       (not (eq this-original-command 'self-insert-command)))))
+             (cua--fallback))))))
+
+   ;; 选择文字后输入文字，不再追加，而是直接替换
+   (delete-selection-mode)
+
+   (show-paren-mode)
+
+   ;; 文件有更改时自动更新
+   (global-auto-revert-mode)
+
+   ;; 按键绑定
+   (global-set-key (kbd "RET") 'newline-and-indent)
+   (global-set-key (kbd "C-<return>") 'newline)
+
+   ;; F4键弹出eshell
+   (global-set-key [f4] 'eshell)
+
+   ;; 按Shift+方向键即可切换窗口
+   (when (fboundp 'windmove-default-keybindings)
+     (windmove-default-keybindings))
+
+   ;; 按C-M-\键格式化
+   (global-set-key (kbd "C-M-\\") 'indent-whole)
+
+   ;; 重写undo
+   (global-undo-tree-mode)))
+
+(add-hook
+ 'window-setup-hook
+ (lambda ()
+   (message "window-setup-hook")
+
+   ;; ido
+   (setq ido-enable-flex-matching t)
+   (setq ido-use-faces nil)
+   (ido-mode)
+   (ido-everywhere)
+   (flx-ido-mode)
+
+   ;; 扩展M-x功能
+   (smex-initialize)
+   (global-set-key (kbd "M-x") 'smex)
+   (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+   ;; 原配的M-x
+   (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+   ;; git管理
+   (add-hook
+    'magit-mode-hook
+    ;;添加gitflow插件
+    (lambda ()
+      (turn-on-magit-gitflow)))
+   (global-set-key [f12] 'magit-status)
+
+   (rainbow-delimiters-mode-enable)
+   (smartparens-strict-mode)
+   (projectile-global-mode)
+   (yas-global-mode)
+
+   ;; 自动补齐
+   (company-mode)
+   (setq company-dabbrev-downcase nil)
+   (setq company-dabbrev-ignore-case nil)
+   (setq company-minimum-prefix-length 1)
+   (setq company-show-numbers t)
+   (company-flx-mode)
+   (global-set-key "\t" 'company-complete-common)
+
+   ;; 语法检查
+   (flycheck-mode)))
+
+;; 自动更换主题
 (after-load "solarized-theme-autoloads"
   (defvar current-theme nil "当前的主题，防止刷新")
   (defun theme-auto-switch ()
@@ -394,81 +320,57 @@
       nil))
   (setq theme-timer (run-with-timer 0 (* 1 60) 'theme-auto-switch)))
 
-;; 在最后一次光标处打开文件
-(after-load "sr-speedbar"
-  (defvar sr-speedbar--target-window
-    (if (not (eq (selected-window) sr-speedbar-window))
-        (selected-window)
-      (other-window 1)))
+;; 全局性设置 end
 
-  (defadvice select-window (after remember-selected-window activate)
-    (unless (or (eq (selected-window) sr-speedbar-window)
-                (not (window-live-p (selected-window))))
-      (setq sr-speedbar--target-window (selected-window))))
+;; go 语言配置
+(add-hook
+ 'go-mode-hook
+ (lambda ()
+   (message "go-mode-hook")
+   (setq gofmt-command "goimports")
+   (add-hook 'before-save-hook 'gofmt-before-save)
 
-  (defun select-next-window ()
-    (select-window sr-speedbar--target-window))
+   (setq company-tooltip-limit 20)
+   (setq company-idle-delay .25)
+   (setq company-echo-delay 0)
+   (setq company-begin-commands '(self-insert-command))
+   (set (make-local-variable 'company-backends) '(company-go))
 
-  ;;Override
-  (defun sr-speedbar-before-visiting-file-hook ()
-    "Function that hooks `speedbar-before-visiting-file-hook'."
-    (select-next-window))
+   (go-eldoc-setup)
 
-  (defun sr-speedbar-before-visiting-tag-hook ()
-    "Function that hooks `speedbar-before-visiting-tag-hook'."
-    (select-next-window))
+   (go-guru-hl-identifier-mode)
 
-  (defun sr-speedbar-visiting-file-hook ()
-    "Function that hooks `speedbar-visiting-file-hook'."
-    (select-next-window))
+   (setq flycheck-disabled-checkers
+         '(go-gofmt
+           go-golint
+           go-vet
+           go-build
+           go-test
+           go-errcheck))
+   (add-hook 'flycheck-mode-hook #'flycheck-gometalinter-setup)))
 
-  (defun sr-speedbar-visiting-tag-hook ()
-    "Function that hooks `speedbar-visiting-tag-hook'."
-    (select-next-window)))
+;; clojure 语言配置
+(add-hook
+ 'clojure-mode-hook
+ (lambda ()
+   (subword-mode)
+   (eldoc-mode)
 
-;; 显示左侧导航，按F9键可以切换
-(after-load "sr-speedbar-autoloads"
-  (global-set-key [f9] 'sr-speedbar-toggle)
-  (setq speedbar-hide-button-brackets-flag t
-        speedbar-show-unknown-files t
-        speedbar-smart-directory-expand-flag t
-        speedbar-directory-button-trim-method 'trim
-        speedbar-use-images nil
-        speedbar-indentation-width 2
-        speedbar-use-imenu-flag t
-        sr-speedbar-width 40
-        sr-speedbar-width-x 40
-        sr-speedbar-auto-refresh nil
-        sr-speedbar-skip-other-window-p t
-        sr-speedbar-right-side nil))
+   (clj-refactor-mode)
+   (cljr-add-keybindings-with-prefix "C-c RET")
 
-;; C-x u开启。p n f b q试试这几个键
-(after-load "undo-tree-autoloads"
-  (global-undo-tree-mode))
+   ;; cider
+   (setq cider-repl-use-clojure-font-lock t)
+   (setq cider-repl-wrap-history t)
+   (setq nrepl-log-messages t)
+   (setq cider-repl-history-size 3000)
+   (setq cider-repl-history-file "~/.emacs.d/cider-history")
+   (setq cider-refresh-show-log-buffer t)
+   (add-hook 'cider-mode-hook #'eldoc-mode)
+   (add-hook 'cider-repl-mode-hook #'subword-mode)
+   (global-set-key (kbd "C-c C-z") 'cider-switch-to-repl-buffer)
 
-(after-load "web-mode-autoloads"
-  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (defun my-web-mode-hook ()
-    "Hooks for Web mode."
-    (setq web-mode-markup-indent-offset 2)
-    (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 2)
-    (setq web-mode-enable-auto-pairing t)
-    (setq web-mode-enable-current-element-highlight t))
-  (add-hook 'web-mode-hook 'my-web-mode-hook))
-
-(after-load "yasnippet-autoloads"
-  (add-hook 'web-mode-hook #'(lambda () (yas-activate-extra-mode 'html-mode)))
-  (add-hook 'clojure-mode-hook #'yas-global-mode)
-  (add-hook 'cider-repl-mode-hook #'yas-global-mode)
-  (add-hook 'emacs-lisp-mode-hook #'yas-global-mode)
-  (add-hook 'go-mode-hook #'yas-global-mode))
+   (flycheck-clojure-setup)))
 
 (provide 'init)
 ;;; init.el ends here
